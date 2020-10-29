dc8f03c988c6c7d0c6bde614e7fec311dad3c5bb
mdeeb2 <59974667+mdeeb2@users.noreply.github.com>
2020-10-29 19:50:25 -0400

    Update budget_schema.js

--- models/budget_schema.js
+++ models/budget_schema.js
@@ -17,7 +17,7 @@ const budgetSchema = new mongoose.Schema({
         unique: true,
         required: true,
         minlength: 7,
-        maxlength: 7 //max length of 7 as hexadecimal format #ffffff is 7 characters long in string but is the 6 digit hexadecimal format
+        maxlength: 7 
     }
 }, { collection: 'items' });
 
